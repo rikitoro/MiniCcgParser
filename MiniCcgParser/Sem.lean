80b66feafa07d7f0df05d273d7e31122223159d4
yoshiaki <yoshiaki@sendai-nct.ac.jp>
2025-09-19 13:12:18 +0900

    Semantics 検討中

--- MiniCcgParser/Sem.lean
+++ MiniCcgParser/Sem.lean
@@ -4,8 +4,17 @@ inductive Ty
   | e
   | t
   | arr (a b : Ty)
+  deriving BEq
 
 infixr:20 " → " => Ty.arr
+
+def Ty.toString : Ty → String
+  | e => "e"
+  | t => "t"
+  | arr a b => a.toString ++ " → " ++ b.toString
+
+instance : ToString Ty where
+  toString := Ty.toString
 
 #check Ty.e → Ty.t
 #check Int → Int
@@ -15,3 +24,28 @@ inductive Term
   | const (c : String) (ty : Ty)
   | lam (x : String) (ty : Ty) (t : Term)
   | app (f a : Term)
+  deriving BEq
+
+def Term.toString : Term → String
+  | var x _ => x
+  | const c _ => c
+  | lam x ty t => x ++ ":" ++ ty.toString ++ "." ++ t.toString
+  | app f a =>
+    let paren s : String := "(" ++ s ++ ")"
+    let fs :=
+      match f with
+      | lam .. => paren f.toString
+      | _ => f.toString
+    let as :=
+      match a with
+      | lam .. | app .. => paren a.toString
+      | _ => a.toString
+    fs ++ as
+
+instance : ToString Term where
+  toString := Term.toString
+
+#eval
+  Term.var "x" Ty.e
+#eval
+  Term.const "hoge" Ty.e
