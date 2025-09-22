a0c0226250bafadf5131de563f3d8b54b39876dc
yoshiaki <yoshiaki@sendai-nct.ac.jp>
2025-09-22 17:46:23 +0900

    update

--- MiniCcgParser/Sem.lean
+++ MiniCcgParser/Sem.lean
@@ -1,51 +1,16 @@
 /- # Semantics -/
 
-inductive Ty : Type
-  | e
-  | t
-  | arr (a b : Ty)
-  deriving BEq
+inductive Ty.e : Type
+  | mk : String → e
 
-infixr:20 " → " => Ty.arr
+#check Ty.e.mk "john"
 
-def Ty.toString : Ty → String
-  | e => "e"
-  | t => "t"
-  | arr a b => a.toString ++ " → " ++ b.toString
+abbrev Ty.t := Prop
 
-instance : ToString Ty where
-  toString := Ty.toString
-
-#check Ty.e → Ty.t
-#check Int → Int
-
-inductive Term
-  | var (x : String) (ty : Ty)
-  | const (c : String) (ty : Ty)
-  | lam (x : String) (ty : Ty) (t : Term)
-  | app (f a : Term)
-  deriving BEq
-
-def Term.toString : Term → String
-  | var x _ => x
-  | const c _ => c
-  | lam x ty t => x ++ ":" ++ ty.toString ++ "." ++ t.toString
-  | app f a =>
-    let paren s : String := "(" ++ s ++ ")"
-    let fs :=
-      match f with
-      | lam .. => paren f.toString
-      | _ => f.toString
-    let as :=
-      match a with
-      | lam .. | app .. => paren a.toString
-      | _ => a.toString
-    fs ++ as
-
-instance : ToString Term where
-  toString := Term.toString
+def Ty.IsJohn : Ty.e → Ty.t
+  | .mk "john"  => True
+  | _           => False
 
 #eval
-  Term.var "x" Ty.e
-#eval
-  Term.const "hoge" Ty.e
+  let john := Ty.e.mk "john"
+  Ty.IsJohn john
